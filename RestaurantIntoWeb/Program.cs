11ab2a04475d5383b7184d31431db22ee1796478
another4433 <alimohamedhassan643@gmail.com>
2025-07-28 11:35:54 +0300

    Preparation for creating a website

---
+++ RestaurantIntoWeb/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+using System.Runtime;
+using Data_Structure;
+
+namespace RestaurantIntoWeb
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Restaurant Management System!");
+            Console.WriteLine("This is a simple console application to manage restaurant operations.");
+            WhatIsATree<string> whatIsATree = new WhatIsATree<string>("Welcome to the restaurant");
+            whatIsATree.add("Menu");
+            whatIsATree.add("Orders");
+            whatIsATree.add("Reservations");
+            whatIsATree.add("Staff");
+            whatIsATree.add("Feedback");
+            whatIsATree.add("Suppliers");
+            whatIsATree.add("Inventory");
+            whatIsATree.printTree();
+            MWArrayList<string> menu = new MWArrayList<string>();
+            menu.add("Pizza");
+            menu.add("Burger");
+            menu.add("Pasta");
+            menu.add("Salad");
+            Console.WriteLine("Menu Items:");
+            menu.printList();
+        }
+    }
+}
