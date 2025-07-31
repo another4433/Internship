b4176263d19447096890b474b455f5d4e1a37fae
Ali Mohamed Ali Hassan <100236390+another4433@users.noreply.github.com>
2025-07-31 14:23:13 +0300

    Made changes to the website with its styling

---
+++ Week 7/Website Design/RestaurantHome.js
@@ -0,0 +1,9 @@
+document.getElementById("first").addEventListener('click', () => {
+    window.location.assign("RestaurantFirstOrder.html");
+});
+document.getElementById("last").addEventListener('click', () => {
+    window.location.assign("RestaurantLastOrder.html");
+});
+document.getElementById("view").addEventListener('click', () => {
+    window.location.assign("RestaurantViewOrder.html");
+});
